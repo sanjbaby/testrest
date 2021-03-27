7492d80ae666c238bacaf99b421f95a313111986
Sanju Baby <sanjbaby@in.ibm.com>
2021-03-27 23:20:37 +0530

    Update run-app.py
    c

--- run-app.py
+++ run-app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import socketapp = Flask(__name__)
+import socket
+
+app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://mongo:27017/sampledb"
 mongo = PyMongo(app)
-db = mongo.db@app.route("/")
+db = mongo.db
+
+@app.route("/")
 def index():
     hostname = socket.gethostname()
     return jsonify(
